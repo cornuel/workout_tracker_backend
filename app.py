df5004eaa7f024a09df94ec4aabfde4ccffb250b
cornuel <ultimn8@gmail.com>
2023-07-06 18:54:37 +0200

    added: validation on signup endpoint

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, jsonify, Blueprint
+from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
-from graphene.types import Boolean
 import graphene
 from pymongo import MongoClient
 from graphql import execute
 from decouple import config
 import logging
 from logging.handlers import RotatingFileHandler
+import re
 
 from workout.schema import schema as workout_schema
 from user_auth.schema import schema as user_auth_schema
@@ -52,6 +52,16 @@ app.logger.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+def validate_email(email):
+    # Email validation regex pattern
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(pattern, email)
+
+def validate_password(password):
+    # Password validation criteria (at least 8 characters and at least one uppercase letter, one lowercase letter, one digit and a symbol)
+    pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+    return re.match(pattern, password)
 
 @app.route("/login", methods=["POST"])
 @cross_origin()
@@ -71,19 +81,30 @@ def login():
 
 @app.route("/signup", methods=["POST"])
 def signup():
-    
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
     email = request.json.get("email", None)
+    
+    # Validate email
+    if not validate_email(email):
+        return jsonify({"msg": "Invalid email format"}), 400
+
+    # Validate password
+    if not validate_password(password):
+        return jsonify({"msg": "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit"}), 400
 
     user = collection.find_one({"username": username})
     if user:
         return jsonify({"msg": "Username already exists"}), 409
+    
+    email = collection.find_one({"email": email})
+    if email:
+        return jsonify({"msg": "Email already exists"}), 409
 
     collection.insert_one({"username": username, "password": password_hash, "email": email})
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
+    return jsonify({"msg": "Account successfully created"}), 200
+
 
 @app.route("/logout", methods=["POST"])
 @jwt_required()
