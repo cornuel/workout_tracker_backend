9159cd0d7039352b9b810d58eae5d89361768c9e
cornuel <ultimn8@gmail.com>
2023-07-24 14:04:56 +0200

    added: resolve_user_exercises

--- exercise/schema.py
+++ exercise/schema.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from decouple import config
+from bson import ObjectId
 from graphene import ObjectType, List, String, Schema
 
 from .models import Exercise, Poses
@@ -10,10 +11,12 @@ client = MongoClient(config('MONGO_URI'))
 db = client["workouttracker"]
 exercises_collection = db["exercises"]
 poses_collection = db["poses"]
+db_user_workouts = client["user_workouts"]
 
 class Query(ObjectType):
     all_exercises = List(Exercise, muscles=List(String))
     all_poses = List(Poses)
+    user_exercises = List(Exercise, user_id=String(required=True), muscles=List(String))
 
     def resolve_all_exercises(self, info, muscles=List(String)):
         query = {}
@@ -32,6 +35,21 @@ class Query(ObjectType):
         for pose in poses_collection.find():
             poses.append(Poses(**pose))
         return poses
+    
+    def resolve_user_exercises(self, info, user_id, muscles=List(String)):
+        query = {}
+        
+        user_collection = db_user_workouts[f"user_{user_id}"]
+
+        if muscles:
+            query = {"exercise.muscles": {"$in": muscles}}
+            
+        exercises_cursor = user_collection.find(query).distinct("exercise")
+
+        exercises = [Exercise(**exercise) for exercise in exercises_cursor]
+
+        return exercises
+
         
 ### Main entry point for the API
 schema = Schema(query=Query)
